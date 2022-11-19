85e66deb22bc05b01531c20031e9ebd5c2646924
Andrei Popa <andypopa@gmail.com>
2022-11-19 15:45:54 +0200

    fix(install.sh): ln syntax

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@ brew list watch || brew install watch
 brew list the_silver_searcher || brew install the_silver_searcher
 git clone https://github.com/andypopa/to-do.git
 cd to-do
-ln -s $(pwd)/to-do /usr/local/bin/to-do
+ln -s /usr/local/bin/to-do $(pwd)/to-do
