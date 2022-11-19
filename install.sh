a109324ee4488f8339dbbfb937c1034a953a99c5
Andrei Popa <andypopa@gmail.com>
2022-11-19 15:31:06 +0200

    feat(install.sh): add check if packages already installed

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
-brew install watch
-brew install the_silver_searcher
+brew list watch || brew install watch
+brew list the_silver_searcher || brew install the_silver_searcher
 git clone https://github.com/andypopa/to-do.git
 cd to-do
 ln -s to-do /usr/local/bin/to-do
